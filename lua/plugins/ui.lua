f3b343cc5b312cf6d3dbf6500cbf8ee886f44675
ExploitEngineer <revilwhoami0098@gmail.com>
2026-01-04 19:12:40 +0500

    feat: add configured incline plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,4 +20,46 @@ return {
       timeout = 10000,
     },
   },
+
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "solarized_dark",
+      },
+    },
+  },
+
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = "NormalFloat",
+            InclineNormalINC = "FloatTitle",
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+]" .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
 }
