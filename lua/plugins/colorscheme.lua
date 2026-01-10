ab59d64ff2db6c61258ee3991635110b8c6ec79f
ExploitEngineer <revilwhoami0098@gmail.com>
2026-01-10 13:02:58 +0500

    feat(ui): add new colors themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,40 @@
+local transparent_opts = {
+  transparent = true,
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  },
+}
+
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    }
-  end,
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = transparent_opts,
+  },
+
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false,
+    opts = transparent_opts,
+  },
+
+  {
+    "vague-theme/vague.nvim",
+    lazy = false,
+    opts = transparent_opts,
+  },
+
+  {
+    "tiagovla/tokyodark.nvim",
+    lazy = false,
+  },
+
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    name = "rose-pine",
+    opts = transparent_opts,
+  },
 }
